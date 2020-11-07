089dad758f7fb89e70af97b3d633d4e3bd9f1784
Munish <munish4humanity@gmail.com>
2020-11-07 13:38:21 +0530

    cHECKING

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 
-app.get('/',function(req,res){
-return res.send('Hellow World');
+app.get('/', function (req, res) {
+    return res.send('Hellow World');
 }
 )
 
-app.listen(3000, function(){
+app.listen(300, function () {
     console.log('App listening on port 3000!');
 })
